2ec3f102df2d3c60dff3fa5019848d035c208280
Guzxtaa <126519312+coala1234@users.noreply.github.com>
2026-05-19 18:03:03 -0300

    Add files via upload

---
+++ Untitled-1.guxtavo12.js
@@ -0,0 +1,43 @@
+let nome = "eduardo";
+ let idade = 18;
+ let cidade = "cascavel";
+ function Tabuada(num){
+    for (let i = 1; i <= 10; i++){
+        console.log(`${num} X ${i} = ${num * i }`);
+    }
+ }
+ function Classificar(n){
+if (n >= 7){
+  return "aprovado";    
+}
+else if (n >= 5){
+    return "recuperação";
+}
+else{ function Classificar(n){
+if (n >= 7){
+  return "aprovado";    
+}
+else if (n >= 5){
+    return "recuperação";
+}
+else{
+    return "reprovado";
+}
+ }
+    return "reprovado";
+}
+ }
+ console.log(`aluno: ${nome}, idade: ${idade}, cidade: ${cidade}`);
+
+ const multiplicacao = (x, y) => x * y;
+ const soma = (x, y) => x + y;
+ const subtracao = (x, y) => x - y;
+ const divisao = (x, y) => x / y;
+
+ console.log(`Soma : ${ soma (10 , 5) } `) ;
+ console.log(`multiplicacao : ${ multiplicacao (10 , 5) } `) ;
+ console.log(`subtração : ${ subtracao (10 , 5) } `) ;
+ console.log(`divisao : ${ divisao (10 , 5) } `) ;
+console.log( Classificar (6))
+
+console.log(Tabuada(2))
